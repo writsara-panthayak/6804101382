aa19c8128c4d4e2e178411febee5bbc3606a237f
writsara panthayak <writsarappp@gmail.com>
2026-08-21 15:34:44 +0700

    v.4-Label with Thai language-382

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page class="q-pa-md">
     <q-form class="q-gutter-md" style="max-width: 600px" @submit="onSubmit" @reset="onReset">
-      <q-input filled v-model="name" label="Your name *" hint="Name and surname" />
-      <q-input filled type="number" v-model="age" label="Your age *" />
+      <q-input filled v-model="name" label="Your name (writsata panthayak)" hint="Name and surname" />
+      <q-input filled type="number" v-model="age" label="Your age (19)" />
       <q-toggle v-model="accept" label="I accept the license and terms" />
       <div>
         <q-btn label="SUBMIT" type="submit" color="primary" />
